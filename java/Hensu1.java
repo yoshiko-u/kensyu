3940e09220fecff5fd9abc2407ad52c1938bf1c1
yoshiko-u <62194599+yoshiko-u@users.noreply.github.com>
2020-04-20 18:28:42 +0900

    Java（本読み進めながら）

---
+++ java/Hensu1.java
@@ -0,0 +1,10 @@
+class Hensu1 {
+    public static void main(String args[]){
+        int num1, num2, num3;
+
+        num1 = 100;
+        num2 = 200;
+        num3 = num1 + num2;
+        System.out.println(num3);
+    }
+}
---
+++ java/Hensu2.java
@@ -0,0 +1,10 @@
+class Hensu2 {
+    public static void main(String args[]){
+        int num1 = 100;
+        int num2 = 200;
+        int num3;
+
+        num3 = num1 + num2;
+        System.out.println(num3);
+    }
+}
