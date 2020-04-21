5a7de9910c9d015aa3882a069a6554220dbff7bc
yoshiko-u <62194599+yoshiko-u@users.noreply.github.com>
2020-04-21 17:16:34 +0900

    テキスト見ながら

---
+++ java/Keisan1.java
@@ -0,0 +1,36 @@
+class Keisan1 {
+    public static void main(String args[]){
+        int num1 = 14;
+        int num2 = 4;
+
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
+
+    }
+}
+
+class Keisan2 {
+    public static void main(String args[]){
+        int num1 = 4;
+        int num2 = 5;
+        int num3 = 6;
+
+        System.out.println(num1 + num2 * num3);
+        System.out.println((num1 + num2) * num3);
+    }
+}
+
+class Keisan3 {
+    public static void main(String args[]){
+        int num1 = 4;
+        num1++;
+        System.out.println(num1);
+        num1++;
+        System.out.println(num1);
+        num1--;
+        System.out.println(num1);
+    }
+}
